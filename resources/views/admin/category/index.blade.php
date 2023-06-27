5380cbc90fdba34043d4504df158f00bee170431
Herry Prasetyo <herry081288@gmail.com>
2023-06-27 23:48:46 +0700

    ok

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.master')
+
+@section('title', 'Category')
+
+@section('breadcrumb')
+
+@endsection
+
+@section('content')
+    <div class="card radius-10">
+        <div class="card-body">
+            <h4>Where does it come from?</h4>
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
+                literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
+                Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur.</p>
+            <h4>Where can I get some?</h4>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in
+                some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing
+                hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined
+                chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200
+                Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks
+                reasonable.</p>
+            <h4>Why do we use it?</h4>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking
+                at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+                as opposed to using 'Content here, content here', making it look like readable English. Many desktop
+                publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for
+                'lorem ipsum' will uncover many web sites still in their infancy.</p>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in
+                some form, by injected humour, or randomised words which don't look even slightly believable. If you are
+                going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the
+                middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as
+                necessary, making this the first true generator on the Internet.</p>
+        </div>
+    </div>
+@endsection
