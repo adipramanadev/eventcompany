ac006c2c069b68b81926498276f2144d00a5991f
Herry Prasetyo <herry081288@gmail.com>
2023-06-27 22:22:34 +0700

    commit branch

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en" class="semi-dark">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @include('admin.components.style')
+    @yield('title')
+</head>
+
+<body>
+
+
+    <!--start wrapper-->
+    <div class="wrapper">
+        <!--start sidebar -->
+        @include('admin.components.sidebar')
+        <!--end sidebar -->
+
+        <!--start top header-->
+        @include('admin.components.header')
+        <!--end top header-->
+
+
+        <!-- start page content wrapper-->
+        <div class="page-content-wrapper">
+            <!-- start page content-->
+            <div class="page-content">
+
+                <!--start breadcrumb-->
+                @yield('breadcrumb')
+                <!--end breadcrumb-->
+
+
+                @yield('content')
+
+
+
+            </div>
+            <!-- end page content-->
+        </div>
+
+
+
+        <!--Start Back To Top Button-->
+        <a href="javaScript:;" class="back-to-top">
+            <ion-icon name="arrow-up-outline"></ion-icon>
+        </a>
+        <!--End Back To Top Button-->
+
+        <!--start switcher-->
+
+        <!--end switcher-->
+
+
+        <!--start overlay-->
+        <div class="overlay nav-toggle-icon"></div>
+        <!--end overlay-->
+
+    </div>
+    <!--end wrapper-->
+
+
+
+    <!-- JS Files-->
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <!--plugins-->
+    <script src="{{ asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+
+    <!-- Main JS-->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+
+
+</body>
+
+</html>
